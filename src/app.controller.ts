c8e6e7080e32c089d216e0a4f480a5451d356821
ShuVeriDa <bashtarov@outlook.com>
2022-11-01 14:09:32 +0300

    setting up jwt

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,21 +1,22 @@
-import { Controller, Get, Post, UseGuards, Request } from '@nestjs/common';
+import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
 import { LocalAuthGuards } from './auth/local-auth.guards';
-import { AuthenticatedGuard } from './auth/authenticated.guard';
+import { AuthService } from './auth/auth.service';
+import { JwtAuthGuard } from './auth/jwt-auth.guard';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly authService: AuthService) {}
 
   @UseGuards(LocalAuthGuards)
   @Post('login')
   login(@Request() req): any {
-    return { msg: 'Logged in' };
+    return this.authService.login(req.user); // TODO: return JWT access Token
   }
 
-  @UseGuards(AuthenticatedGuard)
+  @UseGuards(JwtAuthGuard)
   @Get('protected')
   getHello(@Request() req): string {
-    return req.user;
+    return req.user; //TODO: require an Bearer token, validate token
   }
 }
